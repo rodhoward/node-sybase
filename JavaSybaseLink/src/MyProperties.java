eb917d6496028a6db2ed4dea400e76a9ef242c36
Rod Howard <rod.howard@gmail.com>
2017-01-09 12:24:27 +1100

    Adding propertiy support for jconnect db driver

---
+++ JavaSybaseLink/src/MyProperties.java
@@ -0,0 +1,26 @@
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ *
+ * @author rod
+ */
+public class MyProperties {
+	
+	public Properties properties;
+
+	public MyProperties(String propertyFileName) {
+		properties = new Properties();
+		try {
+			InputStream in =  new FileInputStream(propertyFileName);
+			properties.load(in);
+			in.close();
+		}
+		catch(Exception e) {
+			System.err.println(e);
+		}
+	}
+
+}
